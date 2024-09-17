e7378a27456c99c64df7a75288af6fd0310edfdf
Manikanta-9909 <chmanikanta9909@gmail.com>
2024-09-17 14:18:31 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,1 +1,13 @@
+#include"StringCalculator.h"
+#include <gtest/gtest.h>
 
+TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
+  //Arrange
+  String input="";
+  int expectedValue=0;
+  //ACt
+  int actualValue =add(input);
+  //Assertion
+  ASSERT_EQ(actualValue,expectedValue);
+  
+}
