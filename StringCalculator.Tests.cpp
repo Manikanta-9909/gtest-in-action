fca5603508257d83278ee51b07c882eedf7c0bfc
Manikanta-9909 <chmanikanta9909@gmail.com>
2024-09-17 15:04:37 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,13 +1,58 @@
-#include"StringCalculator.h"
+#include "StringCalculator.h"
 #include <gtest/gtest.h>
 
-TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
+TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected)
+{
   //Arrange
-  String input="";
-  int expectedValue=0;
-  //ACt
-  int actualValue =add(input);
-  //Assertion
-  ASSERT_EQ(actualValue,expectedValue);
+  string input="";
+  int expectedValue = 0;
+  //ACT
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_EQ(actualValue, expectedValue);
+  
+}
+TEST(StringCalculatorTestSuite,add_ZeroInputString_ZeroIsExpected)
+{
+  //Arrange
+  string input= "0";
+  int expectedValue = 0;
+  //ACT
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_EQ(actualValue, expectedValue);
+  
+}
+TEST(StringCalculatorTestSuite,add_OneInputString_OneIsExpected)
+{
+  //Arrange
+  string input= "1";
+  int expectedValue = 1;
+  //ACT
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_EQ(actualValue, expectedValue);
+  
+}
+TEST(StringCalculatorTestSuite,add_NegativeString_NegativeIsExpected)
+{
+  //Arrange
+  string input= "-1";
+ str expectedValue = "Negatives not allowed";
+  //ACT
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_GT(actualValue, expectedValue);
+  
+}
+TEST(StringCalculatorTestSuite,add_CommadelimiterString_AddIsExpected)
+{
+  //Arrange
+  string input= " 1,2";
+ int expectedValue = 3;
+  //ACT
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_GT(expectedValue,actualValue);
   
 }
